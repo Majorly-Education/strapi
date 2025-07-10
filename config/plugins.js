c74b12a73d836d955d951974caead7ac6108786e
jschlanger12 <jschlanger12@gmail.com>
2025-07-10 18:17:29 -0400

    test

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,20 @@
-export default ({ env }) => ({
-  upload: {
-    provider: '@ntegral/strapi-provider-upload-azure-storage-blob',
-    providerOptions: {
-      account:        env('AZURE_STORAGE_ACCOUNT'),
-      accountKey:     env('AZURE_STORAGE_KEY'),
-      serviceBaseURL: env('AZURE_STORAGE_URL'),       
-      containerName:  env('AZURE_STORAGE_CONTAINER'),
-      defaultPath:    'assets',                        
-    },
-    actionOptions: {
-      upload:   {},
-      uploadStream: {},
-      delete:   {},
-    },
-  },
-});
+module.exports = () => ({});
+
+
+// export default ({ env }) => ({
+//   upload: {
+//     provider: '@ntegral/strapi-provider-upload-azure-storage-blob',
+//     providerOptions: {
+//       account:        env('AZURE_STORAGE_ACCOUNT'),
+//       accountKey:     env('AZURE_STORAGE_KEY'),
+//       serviceBaseURL: env('AZURE_STORAGE_URL'),       
+//       containerName:  env('AZURE_STORAGE_CONTAINER'),
+//       defaultPath:    'assets',                        
+//     },
+//     actionOptions: {
+//       upload:   {},
+//       uploadStream: {},
+//       delete:   {},
+//     },
+//   },
+// });
