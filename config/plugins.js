36e2103deb2c8686e99251fb31b0fd649d6970c1
Anurag Pal <apal895@gmail.com>
2025-08-20 07:47:06 +0530

    fix: convert plugins.js from ES6 to CommonJS syntax
    - Changed export default to module.exports to fix Strapi loading error
    - Fixed 'Unexpected token export' error that prevented Strapi from starting
    - Cleaned up formatting and removed commented code

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,17 @@
-// module.exports = () => ({});
-
-
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
-    provider: '@ntegral/strapi-provider-upload-azure-storage-blob',
+    provider: "@ntegral/strapi-provider-upload-azure-storage-blob",
     providerOptions: {
-      account:        env('AZURE_STORAGE_ACCOUNT'),
-      accountKey:     env('AZURE_STORAGE_KEY'),
-      serviceBaseURL: env('AZURE_STORAGE_URL'),       
-      containerName:  env('AZURE_STORAGE_CONTAINER'),
-      defaultPath:    'assets',                        
+      account: env("AZURE_STORAGE_ACCOUNT"),
+      accountKey: env("AZURE_STORAGE_KEY"),
+      serviceBaseURL: env("AZURE_STORAGE_URL"),
+      containerName: env("AZURE_STORAGE_CONTAINER"),
+      defaultPath: "assets",
     },
     actionOptions: {
-      upload:   {},
+      upload: {},
       uploadStream: {},
-      delete:   {},
+      delete: {},
     },
   },
 });
