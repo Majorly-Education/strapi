73ad21c084eafe43a2ff25d939dcd13ac09587b9
Anurag Pal <apal895@gmail.com>
2025-08-21 21:02:23 +0530

    added s3 configuration for media storage and delivery

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,28 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "@ntegral/strapi-provider-upload-azure-storage-blob",
-    providerOptions: {
-      account: env("AZURE_STORAGE_ACCOUNT"),
-      accountKey: env("AZURE_STORAGE_KEY"),
-      serviceBaseURL: env("AZURE_STORAGE_URL"),
-      containerName: env("AZURE_STORAGE_CONTAINER"),
-      defaultPath: "assets",
-    },
-    actionOptions: {
-      upload: {},
-      uploadStream: {},
-      delete: {},
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: env("CDN_URL"),
+        rootPath: env("CDN_ROOT_PATH"),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
